a73de95e4b0d3866908c7b0f8b9623144423df62
CagriSert <56149130+CagriSert@users.noreply.github.com>
2021-01-16 23:46:46 +0300

    Add project files.

---
+++ metodlar/SepetMeneger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace metodlar
+{
+    class SepetMeneger
+    {
+        //name convention
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Sepete Eklendi : "+ urun.Adi);
+        }
+        public void Ekle2(string urunAdi, string aciklama, double fiyat, int adedi)
+        {
+
+        }
+
+    }
+}
---
+++ metodlar/Urun.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace metodlar
+{
+    class Urun
+    {
+
+        //Property - Özellik
+        //prop
+
+
+        public int Id { get; set; }
+
+        public string Adi { get; set; }
+
+        public double Fiyati { get; set; }
+
+        public string Aciklama { get; set; }
+
+        public int StokAdedi { get; set; }
+
+
+    }
+}
